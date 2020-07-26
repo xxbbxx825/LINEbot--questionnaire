f581f604395e834af76b047e8bc4947677381f59
Hiro <xxbbxx825@gmail.com>
2020-07-26 16:25:46 +0900

    linebot

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -35,13 +35,12 @@ class LinebotController < ApplicationController
         end
         docOdakyu = Nokogiri::HTML.parse(htmlOdakyu, nil, charset)
         docOdakyu.xpath('//div[@id="pagettl"]').each do |node|
-        response = 
-          node.css('p').inner_text
+        response = node.css('p').inner_text
         end
       else
         response = "www"
       end
-      
+
       case event
       when Line::Bot::Event::Message
         case event.type
