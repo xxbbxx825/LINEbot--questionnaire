af46465a90ee90309c602da8418746f970a996c6
Hiro <xxbbxx825@gmail.com>
2020-08-02 10:43:04 +0900

    circleci add

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -21,27 +21,11 @@ class LinebotController < ApplicationController
     end
     
     events = client.parse_events_from(body)
-    
+
     events.each { |event|
       require 'capybara/poltergeist'
       require 'open-uri'
       require 'nokogiri'
-      Capybara.register_driver :poltergeist do |app|
-      Capybara::Poltergeist::Driver.new(app, {:js_errors => false, :timeout => 5000,phantomjs_options: [
-                                        '--load-images=no',
-                                        '--ignore-ssl-errors=yes',
-                                        '--ssl-protocol=any'] })
-      end
-      session = Capybara::Session.new(:poltergeist)
-      session.driver.headers = {
-          'User-Agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2564.97 Safari/537.36"
-      }
-      session.visit "https://hazard.yahoo.co.jp/article/covid19osaka"
-      html4 = session.html
-      doc4 = Nokogiri::HTML.parse(html4)
-      pandemic1 = doc4.xpath('//*[@id="box"]/div/dl[1]/div[1]').inner_text
-      pandemic2 = doc4.xpath('//*[@id="box"]/div/dl[1]/div[2]').inner_text
-      pandemic3 = doc4.xpath('//*[@id="box"]/div/dl[1]/div[3]').inner_text
       if event.message["text"].include?("1")
         url1 = 'https://transit.yahoo.co.jp/traininfo/detail/263/0/'
         charset = nil
@@ -72,6 +56,22 @@ class LinebotController < ApplicationController
         response = wheather
 
       elsif event.message["text"].include?("3")
+        Capybara.register_driver :poltergeist do |app|
+        Capybara::Poltergeist::Driver.new(app, {:js_errors => false, :timeout => 5000,phantomjs_options: [
+                                          '--load-images=no',
+                                          '--ignore-ssl-errors=yes',
+                                          '--ssl-protocol=any'] })
+        end
+        session = Capybara::Session.new(:poltergeist)
+        session.driver.headers = {
+            'User-Agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2564.97 Safari/537.36"
+        }
+        session.visit "https://hazard.yahoo.co.jp/article/covid19osaka"
+        html4 = session.html
+        doc4 = Nokogiri::HTML.parse(html4)
+        pandemic1 = doc4.xpath('//*[@id="box"]/div/dl[1]/div[1]').inner_text
+        pandemic2 = doc4.xpath('//*[@id="box"]/div/dl[1]/div[2]').inner_text
+        pandemic3 = doc4.xpath('//*[@id="box"]/div/dl[1]/div[3]').inner_text
         response = "大阪府 新型コロナ関連情報\n" + pandemic1 + "\n" + pandemic2 + "\n" + pandemic3
 
       else
